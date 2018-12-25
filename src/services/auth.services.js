991fedc77840867c16d58f2682f2dcc00f943541
Charles4th <yongn1494@gmail.com>
2018-12-25 14:52:35 +0900

    Add passport-jwt

--- src/services/auth.services.js
+++ src/services/auth.services.js
@@ -1,8 +1,12 @@
 import passport from 'passport';
 import LocalStrategy from 'passport-local';
+import { Strategy as JWTStrategy, ExtractJwt } from 'passport-jwt';
 
 import User from '../modules/users/user.model';
+import constants from '../config/constants';
 
+
+// Local strategy
 const localOpts = {
   usernameField: 'email',
 };
@@ -23,6 +27,28 @@ const localStrategy = new LocalStrategy(localOpts, async (email, password, done)
   }
 });
 
-passport.use(localStrategy);
+// Jwt strategy
+const jwtOpts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeader('authorization'),
+  secretOrKey: constants.JWT_SECRET,
+};
 
-export const authLocal = passport.authenticate('local', { session: false});
+const jwtStrategy = new JWTStrategy(jwtOpts, async (payload, done) => {
+  try {
+    const user = await User.findById(payload._id);
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    return done(null, user);
+  } catch (e) {
+    return done(e, false);
+  }
+});
+
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+export const authLocal = passport.authenticate('local', { session: false });
+export const authJwt = passport.authenticate('jwt', { session: false });
