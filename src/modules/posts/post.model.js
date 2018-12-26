7192679dd6c8c7758c069db770d534e1c9e0c8b2
Charles4th <yongn1494@gmail.com>
2018-12-26 14:56:38 +0900

    Get a post by it's id

--- src/modules/posts/post.model.js
+++ src/modules/posts/post.model.js
@@ -45,6 +45,17 @@ PostSchema.pre('validate', function(next) {
 PostSchema.methods = {
   _slugify() {
     this.slug = slug(this.title);
+  },
+  toJSON() {
+    return {
+      _id: this._id,
+      title: this.title,
+      text: this.text,
+      createAt: this.createdAt,
+      slug: this.slug,
+      user: this.user,
+      favoriteCount: this.favoriteCount
+    }
   }
 };
 
--- src/modules/posts/post.routes.js
+++ src/modules/posts/post.routes.js
@@ -8,5 +8,6 @@ import postValidation from './post.validations';
 const routes = new Router();
 
 routes.post('/', authJwt, validate(postValidation.createPost), postController.createPost);
+routes.get('/:id', postController.getPostById)
 
 export default routes;
